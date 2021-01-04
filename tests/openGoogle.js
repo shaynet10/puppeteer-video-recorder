7954d27d216618562456e83a1591a4166835e6c2
Shay.Gueta <shaynet10@gmail.com>
2021-01-04 22:33:36 +0200

    Some updates to tests and package.json

--- tests/openGoogle.js
+++ tests/openGoogle.js
@@ -2,7 +2,7 @@ const PuppeteerVideoRecorder = require('../index');
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = (await browser.pages())[0];
     const recorder = new PuppeteerVideoRecorder();
     await recorder.init(page, __dirname);
@@ -10,7 +10,7 @@ const puppeteer = require('puppeteer');
     await recorder.start();
     // await page.waitForNavigation({ waitUntil: 'domcontentloaded'});
     const input = await page.$('input[name=q]');
-    await input.type('puppetter-mass-screenshots', { delay: 1000 });
+    await input.type('puppetter-mass-screenshots', { delay: 250 });
     await input.press('Enter');
     // await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
     await recorder.stop();
