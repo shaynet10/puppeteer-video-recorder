3c7ee980e34c62ca7c51969f0147276fd9148378
Johan van Tongeren <info@dreamdealer.nl>
2021-03-31 08:54:09 +0200

    Update FsHandler.js

--- handlers/FsHandler.js
+++ handlers/FsHandler.js
@@ -1,16 +1,36 @@
-const { appendFile, mkdir } = require('fs').promises;
+const { appendFile, mkdir, rmdir, unlink } = require('fs').promises;
 const { openSync, closeSync, existsSync } = require('fs');
 const { join } = require('path');
 
 class FsHandler {
-    async init(outputFolder) {
+    async init(outputFolder, testName) {
+        const now = new Date();
         this.outputFolder = outputFolder;
-        this.videoFilename = join(this.outputFolder, Date.now() + '.webm');
+
+        const hours = this.addZeroBefore(now.getHours());
+        const minutes = this.addZeroBefore(now.getMinutes());
+        const seconds = this.addZeroBefore(now.getSeconds());
+        const videofilename = `${testName.replace(/ /gi, '_').toLowerCase()}-${hours}:${minutes}:${seconds}.webm`;
+        
+        this.videoFilename = join(this.outputFolder, videofilename);
         this.imagesPath = join(this.outputFolder, 'images');
         this.imagesFilename = join(this.outputFolder, 'images.txt');
         await this.verifyPathExists(this.outputFolder);
         await this.verifyPathExists(this.imagesPath);   
         await this.verifyPathExists(this.imagesFilename, 'file');
+    }
+
+    addZeroBefore(n) {
+        return (n < 10 ? '0' : '') + n;
+    }
+
+    async clear() {
+        if (await existsSync(this.imagesPath)) {
+            await rmdir(this.imagesPath, { recursive: true });
+        }
+        if (await existsSync(this.imagesFilename)) {
+            await unlink(this.imagesFilename);
+        }
     }
 
     createEmptyFile(filename) {
