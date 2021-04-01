b46b03f1a4fd9e937f497c3bda91c1c7c5413a1f
Johan van Tongeren <info@dreamdealer.nl>
2021-04-01 16:55:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,26 +11,40 @@ class PuppeteerVideoRecorder {
     async init(page, outputFolder, testName){
         this.page = page;
         this.outputFolder = outputFolder;
-        await this.fsHandler.init(outputFolder, testName);
+        // replace spaces with underscores and form a name for a sub-folder for each test
+        this.testSubFolder = testName.replace(/ /gi, '_').toLowerCase();
+        // append sub-folder name to output folder to form the full output folder name
+        this.fullOutputFolder = `${this.outputFolder.replace(/\/$/, "")}/${this.testSubFolder}/`;
+        await this.fsHandler.init(this.fullOutputFolder, testName);
         const { imagesPath, imagesFilename, appendToFile } = this.fsHandler;
+        // strip the full output foldername from the filename to prevent FFMPEG throwing errors
         await this.screenshots.init(page, imagesPath, {
-            afterWritingImageFile: (filename) => appendToFile(imagesFilename, `file '${filename.replace(outputFolder,'')}'\n`)
+            afterWritingImageFile: (filename) => appendToFile(imagesFilename, `file '${filename.replace(this.fullOutputFolder,'')}'\n`)
         });
     }
 
+    // start recording images
     start(options = {}) { 
         return this.screenshots.start(options);
     }
-    
+
+    // stop recording and save images as videofile
     async stop () {
     	await this.screenshots.stop();
     	await this.createVideo();
-        await this.fsHandler.clear();
     }
 
+    // stop recording, save video and clear the subfolder with images
+    async stopAndClear () {
+    	await this.screenshots.stop();
+    	await this.createVideo();
+        await this.fsHandler.clearOutputSubFolder();
+    }
+
+    // stop recording, do not save a video and clear all images
     async cancel () {
     	await this.screenshots.stop();
-        await this.fsHandler.clear();
+        await this.fsHandler.clearOutputSubFolder();
     }
 
     get defaultFFMpegCommand() {
