769ae82d66784ebdc40ee24a4d7c82a483d2f28b
jvgerb <julia.gorbatch@gmail.com>
2022-08-08 11:50:22 +0300

    feat: added frameRate property for outer consumers

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const { exec } = require('child_process');
 const { promisify } = require('util');
 const PuppeteerMassScreenshots = require('puppeteer-mass-screenshots');
 
-const FRAME_RATE = 42;
+const FRAME_RATE = 36;
 const SIZE = '1080:1920';
 class PuppeteerVideoRecorder {
-    constructor(){
+    constructor(frameRate){
         this.screenshots = new PuppeteerMassScreenshots();
         this.fsHandler = new FsHandler();
+        this.frameRate = frameRate;
     }
 
     async init(page, outputFolder){
@@ -38,7 +39,7 @@ class PuppeteerVideoRecorder {
             '-safe 0',
             `-i ${imagesFilename}`,
             '-bsf:v setts=TS/3',
-            `-r ${FRAME_RATE}`,
+            `-r ${this.frameRate ?? FRAME_RATE}`,
             '-pix_fmt yuv420p',
             `-vf scale="${SIZE}"`,
             videoFilename
